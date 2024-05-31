d0aefdbf275a9c3242229bdb096f6c44be4f0eac
Gustavo Rabelo <gustavo.amorim09@hotmail.com>
2024-05-31 13:54:43 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/projectbancodedados/project/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.projectbancodedados.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projectbancodedados.project.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	
+	
+	
+}
