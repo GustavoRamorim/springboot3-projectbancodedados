53e3b34a8ba1733fdca54e3f948f10354030a906
Gustavo Rabelo <gustavo.amorim09@hotmail.com>
2024-05-31 14:22:52 -0300

    Service layer, component registration

---
+++ src/main/java/com/projectbancodedados/project/services/UserService.java
@@ -0,0 +1,28 @@
+package com.projectbancodedados.project.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projectbancodedados.project.entities.User;
+import com.projectbancodedados.project.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
